30e6738439120b32501d21737394a90922d221f4
Sam Lorimer <slorimerjr@gmail.com>
2021-11-15 01:48:32 +0000

    Finalized

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+
+
+
+
+
 # You must be 120 cm to ride
 # Ticket cost varies based on age (12 and under = 5; 13 - 17 = 7; 18+ = 12)
 # Riders have the option of taking a photo for an additional $3
@@ -7,14 +12,17 @@ height = int(input("How tall are you (cm)? "))
 if height >= 120:
   age = int(input("How old are you? "))
   if age <= 12:
+    print("Child tickets are $5.")
       bill = 5
   elif age <= 17:
+    print("Youth tickets are $7.")
      bill = 7
   else:
+    print("Adult tickets are $12.")
      bill = 12
     
-  picture = input("Would you like to take a picture? Y or N ")
-  if picture == "Y":
+  wants_picture = input("Would you like to take a picture? Y or N ")
+  if wants_picture == "Y":
     bill += 3
 
   print(f"You're total today will be ${bill}.")
