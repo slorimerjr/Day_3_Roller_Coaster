b7a399866e2b7e541eceb3f626469d5f788fbe75
Sam Lorimer <slorimerjr@gmail.com>
2021-11-15 01:39:06 +0000

    Complted

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-# 🚨 Don't change the code below 👇
-year = int(input("Which year do you want to check? "))
-# 🚨 Don't change the code above 👆
+# You must be 120 cm to ride
+# Ticket cost varies based on age (12 and under = 5; 13 - 17 = 7; 18+ = 12)
+# Riders have the option of taking a photo for an additional $3
+print("Welcome to the roller coaster!")
+bill = 0
+height = int(input("How tall are you (cm)? "))
+if height >= 120:
+  age = int(input("How old are you? "))
+  if age <= 12:
+      bill = 5
+  elif age <= 17:
+     bill = 7
+  else:
+     bill = 12
+    
+  picture = input("Would you like to take a picture? Y or N ")
+  if picture == "Y":
+    bill += 3
 
-#Write your code below this line 👇
+  print(f"You're total today will be ${bill}.")
+
+else:
+  print("Sorry, you need to be taller to ride the ride.")
 
 
 
